373cee92cb0c0d6bf41868608f17ccb7fffcb944
azh-1415926 <azhandhj@outlook.com>
2024-09-15 09:05:16 +0800

    加入了开题报告，梳理了一下功能文档，增加了 Qt Webassembly 的构建脚本

---
+++ run.cmd
@@ -0,0 +1,22 @@
+@REM 构建路径
+set BUILD_PATH=./build-wasm
+
+@REM Qt WebAssembly 工具链目录
+set QT_WEBASSEMBLY_TOOLCHAIN=D:/Qt/6.5.3/wasm_singlethread
+
+@REM emsdk 路径
+set EMSDK=D:/WebAssembly/emsdk
+
+set TOOLCHAIN_FILE=%QT_WEBASSEMBLY_TOOLCHAIN%/lib/cmake/Qt6/qt.toolchain.cmake
+
+@REM emsdk 的程序、文件的路径
+set CROSSCOMPILING_EMULATOR=%EMSDK%/node/18.20.3_64bit/bin/node.exe
+set CHAINLOAD_TOOLCHAIN_FILE=%EMSDK%/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+set PYTHONPATH=%EMSDK%/python/3.9.2-nuget_64bit/python.exe
+
+@REM 执行 cmake，构建、编译项目
+cmake -B %BUILD_PATH% -S . -GNinja -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE% -DCMAKE_PREFIX_PATH=%QT_WEBASSEMBLY_TOOLCHAIN% -DCMAKE_CROSSCOMPILING_EMULATOR=%CROSSCOMPILING_EMULATOR% -DQT_CHAINLOAD_TOOLCHAIN_FILE=%CHAINLOAD_TOOLCHAIN_FILE%
+cmake --build %BUILD_PATH% -j8
+
+@REM 启动 WebAssembly 项目
+%PYTHONPATH% %EMSDK%/upstream/emscripten/emrun.py --browser iexplore --port 30000 --no_emrun_detect --serve_after_close %BUILD_PATH%/POCAssistant.html...
